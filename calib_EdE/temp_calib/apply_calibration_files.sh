27959fa2d9d61ede97bb2b92bea0730f17f51544
Alex Nuclear Boy <alex.nuclearboy@gmail.com>
2019-06-12 17:00:41 +0200

    copy CalibCards to Database

---
+++ calib_EdE/temp_calib/apply_calibration_files.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+mkdir $ROOTSORTERSYS/wasa/Database/temp_calib
+cp CalibCardFDScint_FRHdefault_44961_46884 $ROOTSORTERSYS/wasa/Database/temp_calib
+cp CalibCardFDScint_FTHdefault_44961_46884 $ROOTSORTERSYS/wasa/Database/temp_calib
+cp CalibCardFDScint_FWCdefault_44961_46884 $ROOTSORTERSYS/wasa/Database/temp_calib
+
